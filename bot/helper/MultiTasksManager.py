beba0ff9fcd316dd17fe7528c3709ad475bd3867
amiralipoor20072007 <amiralipoor20072007@gmail.com>
2023-03-29 02:58:59 +0330

    deleted debugging comments

--- bot/helper/MultiTasksManager.py
+++ bot/helper/MultiTasksManager.py
@@ -90,7 +90,6 @@ class Multi_Tasks_Manager():
 
     async def downloader_medias(self):
         if self.medias:
-            LOGGER.info(f"medias len : {len(self.medias)}")
             await self.TelegramDownloadHelper.add_download(self.medias.pop(), f'{self.listener.dir}/', "",multi=True)
     
     async def downloader(self):
@@ -109,7 +108,6 @@ class Multi_Tasks_Manager():
     async def run(self):
         self.create_listener()
         await self.get_messages()
-        # LOGGER.info(f"medias : {self.medias}")
         if self.check_not_empty():
             await sendMessage(self.end,"<b>Started To Do Your Tasks One by One!</b>")
             if self.medias:
