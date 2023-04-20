28d6cda2f7ad8a62d32c01c0eefe277e783e0907
amiralipoor20072007 <amiralipoor20072007@gmail.com>
2023-04-20 17:24:08 +0330

    fixing

--- bot/helper/listener.py
+++ bot/helper/listener.py
@@ -290,6 +290,7 @@ class MirrorLeechListener:
             else:
                 fmsg = ''
                 for index, (link, name) in enumerate(files.items(), start=1):
+                    LOGGER.info(f"self.isSuperGroup : {self.isSuperGroup}")
                     if not self.isSuperGroup:
                         Message_id = link.split("/")[-1]
                         LOGGER.info(f"message.from_user.id = {self.message.from_user.id},Message_id = {Message_id}")
